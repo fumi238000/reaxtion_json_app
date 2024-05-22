65d6f2991b302c95e150a20e01fb3f626766c5e7
fumi238000 <s21231128@gmail.com>
2024-05-22 13:01:56 +0900

    fix インデント調整

--- pages/A2.standard.py
+++ pages/A2.standard.py
@@ -93,24 +93,25 @@ def show_specification():
     ### 構成
 
     ```json
-    "mode": "Standard",
-    "name": "スタンダート",
-    "prefer": {
-      "color_num_index": 0, // 色の数
-      "target_num_index": 0, // ターゲット数
-      "delay_index": 2, // 次の点灯までの時間(最短)
-      "delay2_index": 2, // 次の点灯までの時間(最長)
-      "timeout_index": 0, // タイムアウト
-      "onbeeper_index": 1, // 消灯音
-      "offbeeper_index": 0, // 反応音
-      "flash_index": 0, // センサー
-      "sense_index": 0, // 点滅
-      "train_time_index": 3, // トレーニング時間
-      "colors": 32, // 使用する色
-      "start_delay_index": 1, // スタート遅延
-      "ending_index": 0, // 終了
-      "cycles_index": 3, // サイクル
-      "show_graphic": false // グラフィック表示
+    {
+      "mode": "Standard",
+      "name": "スタンダート",
+      "prefer": {
+        "color_num_index": 0, // 色の数
+        "target_num_index": 0, // ターゲット数
+        "delay_index": 2, // 次の点灯までの時間(最短)
+        "delay2_index": 2, // 次の点灯までの時間(最長)
+        "timeout_index": 0, // タイムアウト
+        "onbeeper_index": 1, // 消灯音
+        "offbeeper_index": 0, // 反応音
+        "flash_index": 0, // センサー
+        "sense_index": 0, // 点滅
+        "train_time_index": 3, // トレーニング時間
+        "colors": 32, // 使用する色
+        "start_delay_index": 1, // スタート遅延
+        "ending_index": 0, // 終了
+        "cycles_index": 3, // サイクル
+        "show_graphic": false // グラフィック表示
       }
     }
     ```
